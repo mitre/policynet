514bcdded712c0a5eb31271119421d18117f0cbd
H. Haven Liu <hliu@mitre.org>
2016-06-28 00:28:10 -0400

    fix java build when directory path contains space

--- src/server/java/build.sh
+++ src/server/java/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd $(dirname "$0")/policynet-layout
+cd "$(dirname "$0")/policynet-layout"
 
 # export JAVA_HOME=${JAVA_HOME:-/usr/lib/jvm/java-1.8.0-openjdk.x86_64/}
 echo "---- Begin: Compiling ---"
